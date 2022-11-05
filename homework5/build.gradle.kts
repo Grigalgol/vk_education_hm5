e70c42ec32ca4fe9abdfc93503c6b8585fc1f17f
Gregory <grisha101202@yanex.ru>
2022-11-05 22:55:45 +0300

    add flyway

--- homework5/build.gradle.kts
+++ homework5/build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.flywaydb:flyway-core:9.7.0")
+    implementation("org.postgresql:postgresql:42.5.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
